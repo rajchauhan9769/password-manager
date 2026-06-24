fe422278d21719cb06c3a1695cda602f3b00aa4a
Raj Chauhan <rajchauhan9769@gmail.com>
2026-06-24 08:32:34 +0530

    Made some improvement in code whether the code should return or break and improved some sequece in delete for Yes or No

--- main.py
+++ main.py
@@ -36,9 +36,18 @@ Create a Master Password to secure
 your vault and continue.
 
 ────────────────────────────────────────
-""")
+""")              
+            print("""
+────────────────────────────────────────
+Password Requirements:
+• 8+ characters
+• Uppercase letter
+• Lowercase letter
+• Digit
+• Special character
+──────────────────────────────────────── """)
             attempt_password = 1
-            while attempt_password < 4:
+            while attempt_password <= 3:
                 add_password = input("""
 ────────────────────
 Please add password: 
@@ -62,18 +71,19 @@ Please add password:
                     print("""
 ════════════════════════════════════════
 
->> Status: VALID PASSWORD 
+>> Status: PASSWORD VERIFIED 
 All validation checks passed successfully.
 
 ════════════════════════════════════════""")
                     hash_password = {"master_hash":hashlib.sha256(add_password. encode()).hexdigest()}
                     json.dump(hash_password,file)
                     print("""
-════════════════════════════════════════
->> Status: Password saved successfully.
-════════════════════════════════════════
+════════════════════════════
+>> Status: PASSWORD SAVED
+════════════════════════════
 """)
                     main_menu()
+                    return 
                 else:
                     print(
 """
@@ -129,9 +139,9 @@ def master_password():
     attempt = 0
     while attempt < 3:
         login_password = input("""
-=======================
+───────────────────────
 Enter Master Password: 
-=======================
+───────────────────────
 =>""")
         if login_password == "":
             print("""
@@ -167,24 +177,26 @@ def main_menu():
     #===================
     while True:
         initial_input = input("""
-========================
+════════════════════════
       Main Menu
-========================
-------------------------
+════════════════════════
+────────────────────────
     1. Add Login
-------------------------
+────────────────────────
     2. View Logins
-------------------------
+────────────────────────
     3. Search Login
-------------------------
+────────────────────────
     4. Delete Login
-------------------------
+────────────────────────
     5. Exit
-------------------------
-========================
+────────────────────────
+      
+────────────────────────
 Type 1, 2, 3, 4, or 5 : 
-========================
+────────────────────────
 =>""")
+# ===================================================================================
         #===========================
         #Option Selection Condition
         #===========================
@@ -195,29 +207,46 @@ Type 1, 2, 3, 4, or 5 :
 Enter Website's Name: 
 ═════════════════════
 >""")
-            website_namehandling(webname)
-            #Website Url
+            if not website_namehandling(webname):
+                continue
+# ======================================================================================
+              
+            # ===========
+            # Website Url
+            # ============
             weburl = input("""
 ══════════════════
 Enter Wesites Url: 
 ══════════════════
->""")
-            website_urlhandling(weburl)
-            #Calling "username" Function 
+>""")           
+            if not website_urlhandling(weburl): 
+                    continue
+            
+
+# =======================================================================================
+            # ===========================
+            # Calling "username" Function 
+            # ===========================
             user_name = input("""
 ═══════════════
 Enter Username: 
 ═══════════════
 >""")
-            print(username_handling(user_name))
+            if not username_handling(user_name):
+                continue
             
             time.sleep(0.5)
+# =======================================================================================
             # Requirements
             print("""
-================================================================================
-Password should contain atleast [1 - capital character], [1 - small character],
-[1 - special character] and more than [8 - characters ].
-================================================================================      
+────────────────────────────────────────
+Password Requirements:
+• 8+ characters
+• Uppercase letter
+• Lowercase letter
+• Digit
+• Special character
+────────────────────────────────────────     
 """)
             #========================================
             # Calling "password_handling" Function
@@ -242,18 +271,18 @@ Password should contain atleast [1 - capital character], [1 - small character],
                 print(data)
             else:
                 print(f"""
-============================
+════════════════════════════
         Search Results
-============================
-----------------------------                         
+════════════════════════════
+────────────────────────────                         
 Website Name : {data["Website Name"]}
-----------------------------
+────────────────────────────
 Website Url  : {data["Website Url"]}
-----------------------------
+────────────────────────────
 Username     : {data["Username"]}
-----------------------------
+────────────────────────────
 Password     : {data["Password"]}
-----------------------------
+────────────────────────────
             """)
         elif initial_input == "4":
             #========================================================================================
@@ -268,31 +297,59 @@ Password     : {data["Password"]}
             else:
                 delete(result)
         elif initial_input == "5":
-            print("Thank for coming!!")
+            print("""
+
+════════════════════════════════════════
+
+>> Status: SESSION ENDED
+
+Thank you for using Password Manager.
+
+════════════════════════════════════════
+
+""")
             break
             
         else:
-            print("\n>>Status: INVALID INPUT")
+            print("""
+++++++++++++++++++++++++++++++++++++++++
+
+>> Status: INVALID INPUT
+
+Please select a valid option.
+
+++++++++++++++++++++++++++++++++++++++++
+""")
 
 
 #======================
 # Website Name Feature
 #======================
 def website_namehandling(webname):
-    print(webname.capitalize())
+        if webname.strip() == "":
+            print("Website name cannot be empty.")
+            return False
+        return True 
 
 #============================
 # Website Url Handling Feature
 #============================
 def website_urlhandling(weburl):       #**** Website Url Validation upcoming ****
-    print(weburl)
+        if "." not in weburl:
+            print("Not a Valid url")
+            return False
+        return True
+
+
 
 #======================
 # Username 
 #======================
 def username_handling(user_name):               #****Username Validation upcoming.****
-    return "Username Added Successfully"
-
+        if user_name.strip() == "":
+            print("Username cannot be empty.")
+            return False
+        return True
 
 #============================================
 # Password Confirming and intiating for json
@@ -300,9 +357,13 @@ def username_handling(user_name):               #****Username Validation upcomin
 def password_handling(user_name, webname, weburl):    
     # Confirming Password.
     # Loop for mismatched password until it matches.
-    attempt_password = 0
-    while attempt_password < 3:
-        password = input("Enter Password: ")
+    attempt_password = 1
+    while attempt_password <= 3:
+        password = input("""
+────────────────────
+Enter Password:
+────────────────────
+>""")
         has_upper = False
         has_lower = False
         has_digit = False
@@ -318,11 +379,15 @@ def password_handling(user_name, webname, weburl):
                 has_special = True
         if len(password)>= 8 and has_upper and has_lower and has_special and has_digit:
             print("""
->> Status: CREATED STRONG PASSWORD
+>> Status: PASSWORD VERIFIED
 All validation checks passed successfully.""")
             attempt = 0
             while attempt < 3:
-                re_enter = input("Re-enter Password: ")   
+                re_enter = input("""
+────────────────────
+Re-enter Password:
+────────────────────
+>""")   
                 if password == re_enter:
                     print("Password Added Successfully.")
                     time.sleep(0.5)
@@ -399,7 +464,6 @@ def save_permanent(response, password, user_name, webname, weburl):
             with open("save.json","r") as file:
                 existing_data = json.load(file)
         except (json.JSONDecodeError, FileNotFoundError):
-                with open("save.json","a") as file:
                     existing_data = []
         existing_data.append({
         "Website Name": webname,
@@ -410,8 +474,10 @@ def save_permanent(response, password, user_name, webname, weburl):
         with open("save.json","w") as file:
             json.dump(existing_data,file,indent="\t")
         print("Password Saved Successfully!!")
-    else:
+    elif response == "N":
         print("Your Password is Terminated")
+    else:
+        print("Invalid Input")
 
 
 #===========================================
@@ -425,12 +491,12 @@ def view():
             print(
 f"""
 {data["Website Name"]} :-
-================================
+════════════════════════════════
 Website  : {data["Website Name"]}
 URL     : {data["Website Url"]}
 Username : {data["Username"]}
 Password : {data["Password"]}
-================================""")
+════════════════════════════════""")
     except (json.JSONDecodeError, FileNotFoundError):
         print("No saved credentials found.")
         
@@ -456,22 +522,27 @@ def delete(account_delete):
     try:
         with open("save.json","r") as file :
             datas = json.load(file)
-        print(f"Are you sure you want to delete: {account_delete}")
+        print("""
+────────────────────────────────────────
+DELETE CONFIRMATION
+────────────────────────────────────────
+""")
+        print(f"Selected Account: {account_delete}")
         delete_input = input("Enter [Y] - Yes or [N] - No: ") 
-        for data in datas:
-            if  delete_input not in  ["Y" , "N"]:
+        if delete_input not in  ["Y" , "N"]:
                 print("Wrong input")
-                break
-            if delete_input == "N":
-                print("Delete process was Terminated.")
-                break
+                return
+        if delete_input == "N":
+            print("Delete process was Terminated.")
+            return
+        for data in datas:
             if delete_input == "Y":
                 if data == account_delete:
                     datas.remove(account_delete)
                     with open("save.json","w") as file:
                         json.dump(datas,file,indent="\t")
                     print("Data was deleted successfully.")
-                    break
+                    return
     except(json.JSONDecodeError, FileNotFoundError):
         print("No saved credentials found.")
                 
